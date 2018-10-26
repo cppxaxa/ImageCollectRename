aa98944bbedeb272b9d527550527d10c34b45a4c
Xaxa <Prateek.Xaxa@kla-tencor.com>
2018-10-26 19:35:24 +0530

    first commit

---
+++ ImageCollectRename/ICRUI.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace ImageCollectRename
+{
+    partial class ICRUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(89, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Work in progress";
+            // 
+            // ICRUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(336, 97);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ICRUI";
+            this.Text = "Image Collect Rename";
+            this.Load += new System.EventHandler(this.ICRUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ImageCollectRename/ICRUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageCollectRename
+{
+    public partial class ICRUI : Form
+    {
+        public ICRUI()
+        {
+            InitializeComponent();
+        }
+
+        private void ICRUI_Load(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory);
+            foreach (var item in files)
+            {
+                if (Path.GetExtension(item) == ".jpg")
+                {
+                    Guid guid = Guid.NewGuid();
+                    File.Move(item, guid.ToString() + ".jpg");
+                }
+            }
+
+            Application.Exit();
+        }
+    }
+}
